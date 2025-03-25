0921169ec31a66a012985ea75d57d98b060a1b30
kuyeol <zimop12@gmail.com>
2025-03-25 12:28:07 +0900

    dd

---
+++ mcp/src/main/java/com/example/mcp/TestResource.java
@@ -0,0 +1,21 @@
+package com.example.mcp;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class TestResource
+{
+  private static final String template = "Hello, %s!";
+
+  private final AtomicLong counter = new AtomicLong();
+
+  @GetMapping("/greeting")
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return template+name;
+  }
+}
---
+++ mcp/src/test/java/com/example/mcp/McpApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
